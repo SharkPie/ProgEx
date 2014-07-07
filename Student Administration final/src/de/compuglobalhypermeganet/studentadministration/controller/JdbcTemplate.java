d627e3105b761cb54cfb48c81d0bcccb4672c50f
SharkPie <grueber.dominik@googlemail.com>
2014-07-07 20:38:57 +0200

    nu aber

---
+++ Student Administration final/src/de/compuglobalhypermeganet/studentadministration/controller/JdbcTemplate.java
@@ -0,0 +1,153 @@
+package de.compuglobalhypermeganet.studentadministration.controller;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Properties;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+public class JdbcTemplate {
+	private static JdbcTemplate instance;
+	private MysqlDataSource dataSource;
+	
+	public static JdbcTemplate getInstance(){
+		if(instance == null){
+			instance = new JdbcTemplate();
+		}
+		return instance;
+	}
+	//No more Instance from jdbc template editable from 
+	private JdbcTemplate(){
+		try{
+			
+			//File reading jdbc.properties to hide connection details 
+			Properties jdbcProperties = new Properties();
+			InputStream in = new FileInputStream("jdbc.properties");
+			jdbcProperties.load(in);
+			
+			//Mysql datasource for connecting to the database
+			dataSource = new MysqlDataSource();
+			dataSource.setURL(jdbcProperties.getProperty("url"));
+			dataSource.setUser(jdbcProperties.getProperty("username"));
+			dataSource.setPassword(jdbcProperties.getProperty("password"));
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//Funktion für Connection Test (Statuslabel)
+public Collection<String[]> querys(String eusername, String epassword) {
+		
+	List<String[]> row = new ArrayList<String[]>();
+	if(eusername == null || eusername.length() < 1 || epassword == null || epassword.length()<1){
+		return row;
+	}	//SQL Statement 
+		String sql = "select Student_ID from Student where Student_ID=1";
+		Connection connection = null;
+		ResultSet resultSet = null; 
+		try{
+		Connection con = getDataSource().getConnection();
+		Statement stmt = con.createStatement(); 
+		ResultSet rs = stmt.executeQuery(sql);
+		ResultSetMetaData metaData = rs.getMetaData();
+		int n = metaData.getColumnCount();
+				while(rs.next()){
+					String[] column = new String[n];
+					for (int i = 0; i < column.length; i++) {
+						column[i] = rs.getString(i+1);
+					}
+					row.add(column);
+		}
+		rs.close();
+		con.close();
+		
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return row;
+	}
+	//Function for logging into the database
+	public Collection<String[]> query(String username, String password) {
+		
+		List<String[]> row = new ArrayList<String[]>();
+		if(username == null || username.length() < 1 || password == null || password.length()<1){
+			return row;
+		}
+		//SQL Statement. Checking username, password and matrikation status
+		String sql = "select Student_ID from Student where Last_Name='" + username + "' and Password='" + password + "' and Matrikulation_Status=1";
+		Connection connection = null;
+		ResultSet resultSet = null; 
+		try{
+		Connection con = getDataSource().getConnection();
+		Statement stmt = con.createStatement(); 
+		ResultSet rs = stmt.executeQuery(sql);
+		ResultSetMetaData metaData = rs.getMetaData();
+		int n = metaData.getColumnCount();
+				while(rs.next()){
+					String[] column = new String[n];
+					for (int i = 0; i < column.length; i++) {
+						column[i] = rs.getString(i+1);
+					}
+					row.add(column);
+		}
+		rs.close();
+		con.close();
+		
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return row;
+		
+	}
+
+
+	public synchronized String[][] getEnroledExam(String username, String password) {
+		String[][] row = new String[20][2];
+		if(username == null || username.length() < 1 || password == null || password.length()<1){
+			return row;
+		}
+		
+		
+		
+		String sql = "select s.Last_Name, c.course_name from course as c, student as s join  exam_current as ec on s.student_id = ec.Student_FK_ID where s.Last_Name='"+username+"' and s.Password='"+password+"' and ec.Course_FK_ID = c.Course_ID;"; 
+					
+		Connection connection = null;
+		ResultSet resultSet = null; 
+		try{
+		Connection con = getDataSource().getConnection();
+		Statement stmt = con.createStatement(); 
+		ResultSet rs = stmt.executeQuery(sql);
+		ResultSetMetaData metaData = rs.getMetaData();
+		int n = metaData.getColumnCount();
+		int j = 0;
+				while(rs.next()){
+					String[] column = new String[n];
+					for (int i = 0; i < column.length; i++) {
+						column[i] = rs.getString(i+1);
+					}
+					row[j++] = column;
+		}
+		rs.close();
+		con.close();
+		
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return row;
+	}
+	
+	public MysqlDataSource getDataSource() {
+		return dataSource;
+	}
+	public void setDataSource(MysqlDataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+}
---
+++ Student Administration final/src/de/compuglobalhypermeganet/studentadministration/model/textfieldCL.java
@@ -0,0 +1,14 @@
+package de.compuglobalhypermeganet.studentadministration.model;
+
+import java.awt.Dimension;
+
+import javax.swing.JTextField;
+
+public class textfieldCL extends JTextField {
+	
+	//Textfield Constructor
+	public textfieldCL(){
+	
+		setPreferredSize(new Dimension(100, 25));
+	}
+}
